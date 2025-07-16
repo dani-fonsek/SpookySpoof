66c99d7548d0c17c1e55d8058777263314f799c2
dani-fonsek <daniellafonseca555@gmail.com>
2025-07-16 16:35:32 -0600

    Agregando archivos grandes con Git LFS

--- loscsv.py
+++ loscsv.py
@@ -1,13 +1,3 @@
-import pandas as pd
-
-# Ruta a tu archivo
-ruta_archivo = '/home/user/Desktop/SpookySpoof/Laboratorio/03-02-2018.csv'
-
-# Cargar el CSV
-df = pd.read_csv(ruta_archivo, low_memory=False)
-
-# Reemplazar "benign" por "BENIGN" (ignora mayúsculas/minúsculas)
-df['Label'] = df['Label'].replace(r'(?i)^benign$', 'BENIGN', regex=True)
-
-# Guardar el archivo corregido (puedes sobrescribir o guardar nuevo)
-df.to_csv(ruta_archivo, index=False)
+version https://git-lfs.github.com/spec/v1
+oid sha256:77dd220c61dda6d2f26f254033a8d334cd62e84c1f31be3dedcc2cc0fdc805e1
+size 431
