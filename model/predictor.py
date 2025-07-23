ff33a0d78e97fef089df9dd0c23ebc37fcc10236
dani-fonsek <daniellafonseca555@gmail.com>
2025-07-22 21:59:04 -0600

    Hice el API y ya puede leer las solicitudes (solamente son de prueba, no reales)

---
+++ model/predictor.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:98a8f58e7fd34d58a0c4871e6c3852b52e010676d484da01600693b53cb40e63
+size 319
---
+++ model/predictor_api.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d55757f8f5df77c3bf8345e3e51a9614db8f1e2d0e1d0bb58db92fe0676dcc71
+size 1561
