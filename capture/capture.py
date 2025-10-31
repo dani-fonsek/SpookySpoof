2e6a00fdf7a7c15156f203b70675f777ad4d39eb
dani-fonsek <daniellafonseca555@gmail.com>
2025-10-30 18:48:27 -0600

    Update

--- capture/capture.py
+++ capture/capture.py
@@ -2,31 +2,22 @@ from scapy.all import sniff, wrpcap
 from datetime import datetime
 import os
 
-
-# Carpeta donde guardar los .pcap
+#Carpeta donde guardar los .pcap
 pcap_dir = "pcaps"
 os.makedirs(pcap_dir, exist_ok=True)
 
-
-# Nombre del archivo con fecha/hora
+#Nombre del archivo con fecha/hora
 pcap_file = os.path.join(pcap_dir, f"capture_{datetime.now().strftime('%Y%m%d_%H%M%S')}.pcap")
 
-
-# Interfaz de red (Wi-Fi en la mayoría de Linux)
+#Interfaz de red (LAN cableada)
 interface = "wlp2s0"
-packets = sniff(iface="wlp2s0", count=100)
-
-
 print(f"[INFO] Capturando en interfaz: {interface}")
-print(f"[INFO] Guardando en: {pcap_file}")
 
+#Captura 100 paquetes como prueba
+packets = sniff(iface=interface, count=300)
 
-# Captura 100 paquetes como prueba
-packets = sniff(iface=interface, count=100)
-
-
-# Guardar en archivo pcap
+# uardar en archivo pcap
 wrpcap(pcap_file, packets)
 
-
+print(f"[INFO] Guardando en: {pcap_file}")
 print("[INFO] Captura completada.")
